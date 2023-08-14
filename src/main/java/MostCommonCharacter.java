2614dfde018e63b60988b47e82be21b854951270
Skyler Giannetti <sjg5901@psu.edu>
2023-08-14 03:27:47 +0000

    Most Common Character challenge complete

--- src/main/java/MostCommonCharacter.java
+++ src/main/java/MostCommonCharacter.java
@@ -1,4 +1,7 @@
 
+import java.util.*;
+
+
 public class MostCommonCharacter {
     /**
      * Find the most common character in str.
@@ -8,6 +11,35 @@ public class MostCommonCharacter {
      * @return the most common character within str.
      */
     public char recurringChar(String str) {
-        return ' ';
+
+        // first, create our HashMap
+        Map<Character, Integer> map = new HashMap<>();
+
+        // properly loads values into Map object
+
+        for (char letter : str.toCharArray()) {
+            if (map.containsKey(letter)) {
+                int prev = map.get(letter);
+
+                map.put(letter, prev+1);
+            } else {
+                map.put(letter, 1);
+            }
+        }
+
+
+
+        // find max value so we can match it during iteration
+
+        int maxOccurrence = Collections.max(map.values());
+
+
+        for (Character ch : map.keySet()) {
+            if (map.get(ch) == maxOccurrence) return ch;
+        }
+
+        // should never be reached due to for loop. Added for compilation purposes
+        return str.charAt(0);
     }
+        
 }
